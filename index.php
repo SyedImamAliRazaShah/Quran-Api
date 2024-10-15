818d4d17b7991cc08e0e5b84941d34d558c157de
SyedImamAliRazaShah <sayedalirazashah985@gmail.com>
2024-10-15 17:54:20 +0500

    quran api work

--- index.php
+++ index.php
@@ -16,11 +16,17 @@ $QuranIndex=$response["data"]["surahs"]["references"];
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
    
-  
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri+Quran&family=Aref+Ruqaa&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu:wght@400..700&display=swap" rel="stylesheet">
+
     <style>
-      body{
-      direction:rtl;
-    }
+        body {
+          direction:center;
+            font-family: 'Amiri Quran', serif;
+
+        }
       </style>
   </head>
   <body>
--- readsurah.php
+++ readsurah.php
@@ -12,18 +12,29 @@ $Quran=$response["data"]["ayahs"];
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri+Quran&family=Aref+Ruqaa&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu:wght@400..700&display=swap" rel="stylesheet">
+
     <style>
-        body{
-            direction:rtl;
+        body {
+            direction: rtl;
+            font-family: 'Amiri Quran', serif;
+
         }
+        
+
     </style>
 </head>
 <body>
     
 <?php
 foreach ($Quran as  $value) {
-    echo'<p>'.$value["text"].'</p>';
-    echo'<audio controls  src="'.$value["audio"].'"></audio>';    
+    echo'<p class="ayatnumber">'. $value["numberInSurah"] .'</p>';
+    echo'<p>' . $value["text"] .'</p>';
+    echo' <audio controls class="audio"  src="'.$value["audio"].'"></audio>';  
+
 }
 ?>
 
